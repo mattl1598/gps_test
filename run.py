b05199362bc9238d3a01dd83519e67570c2c8f02
Matthew Larby <37371392+mattl1598@users.noreply.github.com>
2023-05-16 09:11:44 +0100

    done

--- run.py
+++ run.py
@@ -2,10 +2,24 @@
 
 from flask import Flask
 app = Flask(__name__)
+app.gps = ""
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/gps')
+def get_gps():
+    return f"{app.gps}"
+
+
+@app.route('/setgps/<latlng>')
+def get_the_gps(latlng):
+    app.gps = latlng
+    return "200"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
